2309b654ec38e812bda00cb01874adfe68b67324
REDitor <71357600+REDitor@users.noreply.github.com>
2021-12-08 20:27:37 +0100

    i think this should be working, but it isn't XD

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ function addItem() {
     const textarea = document.getElementById('todoText');
 
     // write your solution here
+    const newCard = document.createElement("div");
+    newCard.className = "card";
+    const newCardBody = document.createElement("div");
+    newCardBody.className = "card-body";
 
+    const paragraph = document.createElement("p");
+    const newContent = document.createTextNode(textarea.value);
+
+    //organise card
+    paragraph.appendChild(newContent);
+    newCardBody.appendChild(paragraph);
+    newCard.appendChild(newCardBody);
+
+    //add card to page
+    const currentCard = document.getElementById("card1");
+    document.body.insertBefore(newCard, currentCard);
+
+    //reset the textarea
     textarea.value = '';
 }
