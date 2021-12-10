642cd7125f8651da93b51f3b724927f8de65a155
REDitor <71357600+REDitor@users.noreply.github.com>
2021-12-10 12:23:32 +0100

    done (delete button to be added)

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
 function addItem() {
+    const textarea = document.getElementById("todoText")
 
-    const textarea = document.getElementById('todoText');
+    const itemList = document.getElementById("itemList");
 
-    // write your solution here
+    const newCol = document.createElement("div");
+    newCol.className = "col-md-6 col-xxl-4 pt-4";
+
     const newCard = document.createElement("div");
     newCard.className = "card";
+
     const newCardBody = document.createElement("div");
     newCardBody.className = "card-body";
 
     const paragraph = document.createElement("p");
-    const newContent = document.createTextNode(textarea.value);
+    paragraph.innerHTML = textarea.value;
 
-    //organise card
-    paragraph.appendChild(newContent);
     newCardBody.appendChild(paragraph);
     newCard.appendChild(newCardBody);
+    newCol.appendChild(newCard);
 
-    //add card to page
-    const currentCard = document.getElementById("card1");
-    document.body.insertBefore(newCard, currentCard);
+    itemList.appendChild(newCol);
+
+    textarea.value = '';
+    // write your solution here
+    // const newCard = document.createElement("div");
+    // newCard.className = "card";
+    // const newCardBody = document.createElement("div");
+    // newCardBody.className = "card-body";
+    //
+    // const paragraph = document.createElement("p");
+    // const newContent = document.createTextNode(textarea.value);
+    //
+    // //organise card
+    // paragraph.appendChild(newContent);
+    // newCardBody.appendChild(paragraph);
+    // newCard.appendChild(newCardBody);
+    //
+    // //add card to page
+    // const row = document.getElementsByClassName('card');
+    // document.body.insertBefore(newCard, row);
 
     //reset the textarea
-    textarea.value = '';
+
 }
